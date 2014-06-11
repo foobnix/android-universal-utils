e7e1f4d8ef44de464226a251e3ff2c617f05a741
ivan.ivanenko <ivan.ivanenko@tsbua.com>
2014-06-11 13:09:09 +0300

    demo commit

---
+++ src/com/foobnix/android/utils/LOG.java
@@ -0,0 +1,38 @@
+package com.foobnix.android.utils;
+
+import android.util.Log;
+
+public class LOG {
+    public static boolean isEnable = true;
+    public static String TAG = "DEBUG";
+    public static String DELIMETHER = "|";
+
+    public static void printlog(String statement) {
+        if (isEnable) {
+            Log.d(TAG, statement);
+        }
+    }
+
+    public static void d(Object... statement) {
+        if (isEnable) {
+            Log.d(TAG, asString(statement));
+        }
+    }
+
+    public static void e(Throwable e, Object... statement) {
+        if (isEnable) {
+            Log.e(TAG, asString(statement), e);
+        }
+    }
+
+    private static String asString(Object... statements) {
+        StringBuilder b = new StringBuilder();
+        for (Object str : statements) {
+            b.append(str);
+            b.append(DELIMETHER);
+        }
+        String string = b.toString();
+        return string;
+    }
+
+}
