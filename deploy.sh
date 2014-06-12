ccfec0eb28b0cc5e6cc050b2375b670c07da17ff
ivan.ivanenko <ivan.ivanenko@tsbua.com>
2014-06-12 14:44:35 +0300

    add jar

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 ./build.sh
-cp build/libs/android-universal-utils.jar /home/ivan/git/mdo360-android/MDO360/libs
+cp -rf build/libs/android-universal-utils.jar /home/ivan/git/mdo360-android/MDO360/libs
+cp -rf build/libs/android-universal-utils.jar lib
