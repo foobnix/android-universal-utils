caa5d4a64fe0fb40db57b926d3f2d10c3322e4bd
ivan.ivanenko <ivan.ivanenko@tsbua.com>
2014-07-16 15:54:14 +0300

    udptees

--- src/com/foobnix/android/utils/Bundles.java
+++ src/com/foobnix/android/utils/Bundles.java
@@ -3,6 +3,7 @@ package com.foobnix.android.utils;
 import android.os.Bundle;
 
 public class Bundles {
+    private final Bundle bundle = new Bundle();
 
     public static Bundle bInt(String key, int value) {
         Bundle bundle = new Bundle();
@@ -16,5 +17,23 @@ public class Bundles {
         return bundle;
     }
 
+    public Bundles add(String key, int value) {
+        bundle.putInt(key, value);
+        return this;
+    }
+
+    public Bundles add(String key, String value) {
+        bundle.putString(key, value);
+        return this;
+    }
+
+    public Bundles add(String key, boolean value) {
+        bundle.putBoolean(key, value);
+        return this;
+    }
+
+    public Bundle build() {
+        return bundle;
+    }
 
 }
