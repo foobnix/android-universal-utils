09e5ed548c411a278e93b69c62c0829361fabef4
ivan.ivanenko <ivan.ivanenko@tsbua.com>
2014-09-04 18:51:06 +0300

    updates

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,7 @@ cp -rf build/libs/android-universal-utils.jar /home/ivan/git/mdo360-android/MDO3
 cp -rf build/libs/android-universal-utils.jar lib
 cp -rf build/libs/android-universal-utils.jar /home/ivan/dev/workspace/mdo360/X_TestProject/libs
 cp -rf build/libs/android-universal-utils.jar /home/ivan/git/ibidsmartapp-android/BuyerApp/libs
+cp -rf build/libs/android-universal-utils.jar /home/ivan/git/ebooka-all/PDFWrapper/libs
+cp -rf build/libs/android-universal-utils.jar /home/ivan/git/ibidsmartcheckin-android/libs
+
+
---
+++ src/com/foobnix/android/utils/ModelActivity.java
@@ -0,0 +1,44 @@
+package com.foobnix.android.utils;
+
+import java.io.Serializable;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public abstract class ModelActivity<T extends Serializable> extends Activity {
+    public static final String EXTRA_FRAGMENT_MODEL = "model";
+    protected T model;
+    protected Handler handler;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handler = new Handler();
+    }
+
+    public void prepareModelFromArgumentOrBundle(Bundle savedInstanceState, Class<T> clazz) {
+        if (getIntent() != null && getIntent().hasExtra(EXTRA_FRAGMENT_MODEL)) {
+            model = (T) getIntent().getSerializableExtra(EXTRA_FRAGMENT_MODEL);
+        }
+        if (model == null && savedInstanceState != null) {
+            model = (T) savedInstanceState.getSerializable(EXTRA_FRAGMENT_MODEL);
+        }
+        if (model == null) {
+            try {
+                model = clazz.newInstance();
+            } catch (Exception e) {
+                LOG.e(e);
+            }
+        }
+    }
+
+    public abstract void populateModel();
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putSerializable(EXTRA_FRAGMENT_MODEL, model);
+        super.onSaveInstanceState(outState);
+    }
+
+}
