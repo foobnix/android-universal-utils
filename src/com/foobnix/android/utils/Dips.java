3cf01533ffe14475402195f5efb5a56c5a610ea8
Ivan Ivanenko <ivan.ivanenko@gmail.com>
2015-05-02 21:12:23 +0300

    update dips

--- src/com/foobnix/android/utils/Dips.java
+++ src/com/foobnix/android/utils/Dips.java
@@ -3,10 +3,7 @@
  */
 package com.foobnix.android.utils;
 
-import android.content.Context;
 import android.content.res.Resources;
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
 
 public class Dips {
 
@@ -18,18 +15,12 @@ public class Dips {
         return (int) (px / Resources.getSystem().getDisplayMetrics().density);
     }
 
-    public static int screenWidth(final Context context) {
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics metrics = new DisplayMetrics();
-        wm.getDefaultDisplay().getMetrics(metrics);
-        return metrics.widthPixels;
+    public static int screenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
     }
 
-    public static int screenHeight(final Context context) {
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics metrics = new DisplayMetrics();
-        wm.getDefaultDisplay().getMetrics(metrics);
-        return metrics.heightPixels;
+    public static int screenHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
     }
 
 
